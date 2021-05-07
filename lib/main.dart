242ae1ad28810c0a58c528668784a9b728d85cff
Frederik <frederik.froeling@web.de>
2021-05-07 15:56:14 +0200

    feat: add stack history, pop behaviour and remove complete stack index functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tab_bar/page1_1.dart';
-import 'package:tab_bar/page1_2.dart';
-import 'package:tab_bar/stacks.dart';
+import 'package:tabtest/page1_1.dart';
+import 'package:tabtest/page1_2.dart';
+import 'package:tabtest/stacks.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,22 +23,70 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   int currentIndex = 0;
+  List<int> stackHistory = [];
 
   @override
   void initState() {
     super.initState();
-    Stacks.init(2);
+    Stacks.init(2, this);
     Stacks.stacks[0].add(Page1_1(index: 0));
     Stacks.stacks[1].add(Page1_2(index: 1));
+    stackHistory.add(0);
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      if (currentIndex == index) {
+        Stacks.popToFirst(index);
+      } else {
+        currentIndex = index;
+      }
+    });
+    //add current index as last item in to stack history
+    stackHistory.remove(index);
+    stackHistory.add(index);
+  }
+
+  popStackHistory() {
+    print("popping Stack history $stackHistory");
+    if (stackHistory.length >= 1) {
+      stackHistory.removeLast();
+    }
+    setState(() {
+      currentIndex = stackHistory.last;
+    });
+  }
+
+  Future<bool> _onWillPop() {
+    if (!Stacks.pop(currentIndex)) {
+      popStackHistory();
+    }
+  }
+
+  refresh() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stacks.stacks[currentIndex].last;
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: Stacks.stacks[currentIndex].last,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("tab1")),
+            BottomNavigationBarItem(icon: Icon(Icons.deck), title: Text("tab2"))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/stacks.dart
+++ lib/stacks.dart
@@ -2,24 +2,39 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 class Stacks {
   static List<Queue> stacks = <Queue>[];
-
-  static init(length) {
+  static MyHomePageState homeState;
+  static init(length, home) {
+    homeState = home;
     if (stacks.length == 0)
       for (int i = 0; i < length; i++) {
         stacks.add(Queue<Widget>());
       }
   }
 
-  static pop(stackNumber) {
-    if (stacks[stackNumber].isEmpty) {
-      throw StateError("Cannot pop() on empty stack.");
+  static bool pop(stackNumber) {
+    print(
+        "popping stacknum $stackNumber with len ${stacks[stackNumber].length}");
+    if (stacks[stackNumber].length <= 1) {
+      return false;
     }
     stacks[stackNumber].removeLast();
+    homeState.refresh();
+    return true;
+  }
+
+  static popToFirst(stackNumber) {
+    for (int i = 0; i < stacks[stackNumber].length - 1; i++) {
+      stacks[stackNumber].removeLast();
+    }
   }
 
   static push(stackNumber, route) {
+    print('adding to ' + stackNumber.toString());
     stacks[stackNumber].addLast(route);
+    homeState.refresh();
   }
 }
